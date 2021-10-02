c11aefde21fd821999f9d528bc5ac4e76091e247
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-02 18:32:00 +0600

    userapp url done

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import *
 from . import views
 urlpatterns = [
-    # path('', home, name='home'),
-
+    path('signup', signup, name='signup'),
+    path('login', login, name='login'),
+    path('forget-password', forget_password, name='forget_password'),
+    path('reset-password', reset_password, name='reset_password'),
+    path('change-password', change_password, name='change_password'),
+    path('user-dashboard', userDashboard, name='user_dashboard'),
+    path('update-profile', updateProfile, name='update_profile'),
 ]
