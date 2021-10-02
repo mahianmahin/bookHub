1d1d38941abbcdd79f5842de1bc5be43e35e4d26
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-02 18:47:03 +0600

    url setup done

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def signup(request):
+    context = {}
+    return render(request, 'signup.html', context=context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'login.html', context=context)
+
+
+def forget_password(request):
+    context = {}
+    return render(request, 'forget-password.html', context=context)
+
+
+def reset_password(request):
+    context = {}
+    return render(request, 'reset-password.html', context=context)
+
+
+def userDashboard(request):
+    context = {}
+    return render(request, 'dashboard.html', context=context)
+
+
+def updateProfile(request):
+    context = {}
+    return render(request, 'dashboard.html', context=context)
