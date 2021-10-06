b60dedcc5015d5c7cafd8be7747b6b0ed2c57e7c
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2021-10-06 12:37:20 +0600

    login fixed

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import *
+
 from . import views
+from .views import *
+
 urlpatterns = [
     path('signup', signup, name='signup'),
-    path('login', login, name='login'),
+    path('login', app_login, name='login'),
     path('forget-password', forget_password, name='forget_password'),
     path('reset-password', reset_password, name='reset_password'),
     path('user-dashboard', userDashboard, name='user_dashboard'),
