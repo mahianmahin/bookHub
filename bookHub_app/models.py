8eb92eb6737f2879de0005d9876522ab14c5311c
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-10 12:13:33 +0600

    database added

--- bookHub_app/models.py
+++ bookHub_app/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE())
+    name = models.CharField(max_length=30,null=True, blank=True)
+    category = models.Choices()
+    description = models.CharField(max_length=250,null=True, blank=True)
+    upload_time = models.DateTimeField()
+    writer_name = models.CharField(max_length=50,null=True, blank=True)
+
+class BooksReview(models.Model):
+    book = models.ForeignKey(Books, on_delete=models.CASCADE())
+    user = models.ForeignKey(User, on_delete=models.CASCADE())
+    review = models.CharField(max_length=250, null=True, blank=True)
+    star = models.IntegerField()
+
+class Blogs(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE())
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250, null=True, blank=True)
+    img = models.ImageField()
+    created_time = models.DateTimeField()
--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE())
+    profile_pic = models.ImageField(null=True, blank=True)
+    address = models.CharField(max_length=50,null=True, blank=True)
+    occupation = models.CharField(max_length=50,null=True, blank=True)
+    bio = models.TextField(max_length=255, null=True, blank=True)
