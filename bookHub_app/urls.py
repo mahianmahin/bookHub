8e7afa4574305691b8ba33cffc87ea19f81b7ead
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2021-10-10 18:13:10 +0600

    progress made

--- bookHub_app/urls.py
+++ bookHub_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import *
-from . import views
-urlpatterns = [
-    path('', home, name='home'),
 
+from . import views
+from .views import *
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('books/', books, name="books"),
+    path('blogs/', blogs, name="blogs"),
+    path('my_blog/', my_blog, name="my_blog"),
+    path('contact/', contact, name="contact"),
+    path('about/', about, name="about"),
 ]
--- bookHub_app/views.py
+++ bookHub_app/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
 def home(request):
-    context = {}
+    context = {
+        "home": "active"
+    }
     return render(request, 'home.html', context)
+
+def books(request):
+    context = {
+        "books": "active"
+    }
+    return render(request, 'books.html', context=context)
+
+def blogs(request):
+    context = {
+        "blogs": "active"
+    }
+    return render(request, 'all-blogs.html', context)
+
+def my_blog(request):
+    if request.user.is_authenticated:
+        context = {
+            "my_blog": "active"
+        }
+        return render(request, 'blog.html', context)
+    
+    else:
+        messages.warning(request, "You must be logged in")
+        return redirect('/login')
+
+def contact(request):
+    context = {
+        "contact": "active"
+    }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        "about": "active"
+    }
+    return render(request, 'about-us.html', context)
