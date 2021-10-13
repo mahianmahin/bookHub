95f8257dd90f8fc221f06d28090fe6c6a35422fd
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-13 08:27:46 +0600

    user profile bug fix.

--- user_app/views.py
+++ user_app/views.py
@@ -30,7 +30,7 @@ def signup(request):
                                         password=password)
         user.save()
         # setup userProfile object
-        user_profile = UserProfile(user_id=user.id)
+        user_profile = UserProfile(user_id=user.id, occupation='occupation', bio='bio', address='address')
         user_profile.save()
         messages.success(request, "Registered Successfully!")
 
@@ -180,6 +180,7 @@ def reset_password(request):
 
 def userDashboard(request):
     if request.user.is_authenticated:
+        global user_profile_ins
         user_profile_ins = UserProfile.objects.filter(user_id=request.user.id).first()
         if request.method == "POST":
             if 'personal_info' in request.POST:
@@ -188,7 +189,6 @@ def userDashboard(request):
                 l_name = request.POST.get('l_name')
                 user_ins = User.objects.get(id=request.user.id)
                 if (f_name == ""):
-                    print("true")
                     user_ins.first_name = user_ins.first_name
                 else:
                     user_ins.first_name = f_name
@@ -203,18 +203,23 @@ def userDashboard(request):
                 address = request.POST.get('address')
                 occupation = request.POST.get('occupation')
                 bio = request.POST.get('bio')
-                # pro_pic = request.FILES['pro_pic']
-                # user Profile instance
-                user_profile_ins_update = UserProfile.objects.filter(user_id=request.user.id).update(
-                    occupation=occupation, bio=bio, address=address)
-
-                # user_profile_ins.occupation = occupation
-                # user_profile_ins.address = address
-                # user_profile_ins.bio = bio
-                # if (pro_pic):
-                #     user_profile_ins.profile_pic = pro_pic
-                # update userProfile.
-                # user_profile_ins.save()
+                if (address == ""):
+                    user_profile_ins.address = user_profile_ins.address
+                else:
+                    user_profile_ins.address = address
+                if (occupation == ""):
+                    user_profile_ins.occupation = user_profile_ins.occupation
+                else:
+                    user_profile_ins.occupation = occupation
+                if (bio == ""):
+                    user_profile_ins.bio = user_profile_ins.bio
+                else:
+                    user_profile_ins.bio = bio
+                if 'image' in request.FILES:
+                    user_profile_ins.profile_pic = request.FILES['image']
+                else:
+                    user_profile_ins.profile_pic = user_profile_ins.profile_pic
+                user_profile_ins.save()
 
             if 'book_upload' in request.POST:
                 book_name = request.POST.get('book_name')
